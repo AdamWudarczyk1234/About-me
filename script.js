7e70beea7011e5ddcb912999c56e7bd9f125183e
AdamWudarczyk1234 <vodo22@gmail.com>
2019-10-21 20:23:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function myfunction() {
+    document.getElementById("textbox").innerHTML = "Text example";
+  }
+
+function myfunction2() {
+  document.getElementById("textbox").innerHTML = "Text example2";
+  }
+
+function myfunction3() {
+  document.getElementById("textbox").innerHTML = "Text example3";
+    }
+
+function myfunction4() {
+  document.getElementById("textbox").innerHTML = "Text example4";
+      }
+
+function myfunction5() {
+  document.getElementById("textbox").innerHTML = "Text example5";
+            }
+
