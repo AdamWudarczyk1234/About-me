1105adf0839459795d9e9a03f5cbcb6397131268
AdamWudarczyk1234 <vodo22@gmail.com>
2019-10-22 21:45:54 +0200

    change position of textbox to position.relative

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function myfunction() {
-    document.getElementById("textbox").innerHTML = "Text example";
+    document.getElementById("textbox").innerHTML = "Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    Na codzień gram na gitarze i śpiewam\
+    ";
   }
 
 function myfunction2() {
@@ -12,9 +19,5 @@ function myfunction3() {
 
 function myfunction4() {
   document.getElementById("textbox").innerHTML = "Text example4";
-      }
-
-function myfunction5() {
-  document.getElementById("textbox").innerHTML = "Text example5";
             }
 
